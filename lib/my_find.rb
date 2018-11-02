8475abdda91052fef0dc8e5d8f608720f6d657ba
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 00:08:57 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,8 +5,9 @@ def my_find(collection)
  new_collection = [] 
  while i < collection.length 
   yield(collection[i])
-  # that new number/item is passed through and then incremented upon
+  # that new number/item is passed in as the argument and then incremented upon
     i = i + 1 
     # iterating by increment 
+    # send in the number to the block, evaluates it, and sends the return value
  end 
 end
