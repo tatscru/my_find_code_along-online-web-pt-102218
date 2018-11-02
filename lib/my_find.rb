c4d625d56104c35bc383766c34842cdf5e55e154
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 00:15:25 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,14 +1,13 @@
 require 'pry'
 
 def my_find(collection)
+  # looking for first return value that is true 
  i = 0
  new_collection = [] 
  while i < collection.length 
-  if yield(collection[i])
+ return collection[i] if yield(collection[i])
   # that new number/item is passed in as the argument and then incremented upon
-    return collection[i]
     # if found true then end the loop, if not continue through
-  end 
     i = i + 1 
     # iterating by increment 
     # send in the number to the block, evaluates it, and sends the return value
